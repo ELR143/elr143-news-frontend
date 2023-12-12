4320df979615d34b3ae8b6eaa52d1e8212eeb689
ELR143 <emmlou143@gmail.com>
2023-12-12 16:29:31 +0000

    render comments with each article

--- src/pages/ArticleByIdPage.jsx
+++ src/pages/ArticleByIdPage.jsx
@@ -1,9 +1,12 @@
+import React from "react";
 import ArticleByIdCard from "../components/ArticleByIdCard";
+import CommentsContainer from "../components/CommentsContainer";
 
 export default function ArticleByIdPage() {
   return (
     <section>
       <ArticleByIdCard />
+      <CommentsContainer />
     </section>
   );
 }
